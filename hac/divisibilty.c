4ba5ce919603eae8d708517300f06215830d5a9f
Prakash Pandey <prakashpandey00002@gmail.com>
2021-08-30 19:45:26 +0530

    Add files via upload
    https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/divisible-or-not-81b86ad7/

---
+++ hac/divisibilty.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            prakash pandey 
+                            cochin university
+*/
+
+#include <stdio.h>
+
+int main(){
+	int N = 0;
+	scanf("%d", &N);
+	
+	long data[N];
+	for(auto i=0; i<N; i++)
+	    scanf("%ld", &data[i]);
+	
+    // write your code here
+    // ans = 
+    printf("%s", ans);
+    
+    return 0;
+}
